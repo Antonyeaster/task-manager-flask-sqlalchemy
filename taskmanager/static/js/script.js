34545d799ee4d4c0eb9f19454d11aec04be07345
Antonyeaster <>
2023-10-16 07:36:38 +0000

    Add ability to create task using a form

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(sidenav);
+    M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "select"}
+    });
+    
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
